a934df8bea49a2adb56f360181892fa821aaac16
Atul625-py <atulpandey2005hbd@gmail.com>
2025-09-20 23:22:15 +0530

    Initial commit - Spring Boot backend setup

---
+++ backend/src/main/java/com/rkvk/automobile/automobileshop/AutomobileshopApplication.java
@@ -0,0 +1,13 @@
+package com.rkvk.automobile.automobileshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomobileshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomobileshopApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/rkvk/automobile/automobileshop/AutomobileshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkvk.automobile.automobileshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobileshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
