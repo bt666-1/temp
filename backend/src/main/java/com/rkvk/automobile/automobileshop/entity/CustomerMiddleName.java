06b2e07c5dd42ce2d2c104b51029795b981eb71e
bt666-1 <thakkarwadbasweshwar@gmail.com>
2025-09-22 19:58:24 +0530

    Update CustomerMiddleName.java
    Fixed foreign key

--- backend/src/main/java/com/rkvk/automobile/automobileshop/entity/CustomerMiddleName.java
+++ backend/src/main/java/com/rkvk/automobile/automobileshop/entity/CustomerMiddleName.java
@@ -1,28 +1,32 @@
 package com.rkvk.automobile.automobileshop.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.rkvk.automobile.automobileshop.entity.id.CustomerMiddleNameId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "customer_middle_name")
+@Table(name = "customer_middle_names")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@IdClass(CustomerMiddleNameId.class)
 public class CustomerMiddleName {
-    @Id
-    private Long customerId;
 
-    @Id
-    private String middleName;
+    // Composite primary key
+    @EmbeddedId
+    private CustomerMiddleNameId id;
 
+    // Extra attribute
     private int middleNameOrder;
+
+    // Foreign key to Customer
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("customerId")
+    @JoinColumn(name = "customer_id", nullable = false)
+    @JsonBackReference
+    private Customer customer;
 }
